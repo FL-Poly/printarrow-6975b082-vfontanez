713a25c3400826bcb4db18c7ca597d51b9fde1f6
vfontanez <89874296+vfontanez@users.noreply.github.com>
2025-01-16 20:49:03 -0500

    Update printarrow.c

--- .github/workflows/printarrow.c
+++ .github/workflows/printarrow.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  int body, head;
+
+// User's Input
+printf("Enter the number for arrow body: ");
+scanf("%d", &body);
+
+printf("Enter the number for arrow head: ");
+scanf("%d" , &head);
+
+//Print Upper Part of the Head
+printf(" d%\n", head);
+printf(" %d%d\n", head, head);
+
+//Print body and Middle Part
+for (int i = 0; i< 3; i++) {
+    printf("%d%d%d%d%d%d%d\n", body, body, body, body, head, head, head);
+}
+
+//Print Lower Part
+printf(" %d%d\n", head, head);
+printf(" %d\n", head);
+
+
+return 0;
+
+}
 
